7557e64cfae803567e1eee8e6a7e07cceb71a36b
Saul Gausin <saul.gausin@gmail.com>
2013-09-18 12:54:30 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialServicePlatform::Application.config.secret_token = '2670ce33ea2e79f8ca7c327905a72e6cf956b1a6bc87e8e020daf28e43a40210c714588dadde1fdac42e569fc4524621ed01286165e75666672b41db4605c2af'
